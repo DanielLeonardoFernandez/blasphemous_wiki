e68ce5b5d4fb673384bc3218d9f3f8c7e72c3ae7
Daniel Leonardo Fernandez Gomes <dlfernandez74@ucatolica.edu.co>
2025-11-30 13:24:08 -0500

    Html fist steps and templates applied, requeriments update, css directory created

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from db import create_tables
 from routers import items, categorias, ubicaciones, interacciones, imagenes
 
 app = FastAPI(lifespan=create_tables, title="Blasphemous Wiki API")
+
+#  Static y Templates
+
+# Servir archivos estáticos (CSS, imágenes, etc.)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Motor de plantillas Jinja2
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(items.router)
 app.include_router(categorias.router)
@@ -10,11 +22,29 @@ app.include_router(ubicaciones.router)
 app.include_router(interacciones.router)
 app.include_router(imagenes.router)
 
-@app.get("/")
-async def root():
-    return {"message": "Bienvenido a la Wiki de Blasphemous"}
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hola {name}, bienvenido a la wiki!"}
 
+@app.get("/hello/{name}", response_class=HTMLResponse)
+async def say_hello(request: Request, name: str):
+    return templates.TemplateResponse(
+        "hello.html",
+        {"request": request, "texto": name.upper()}
+    )
+
+# -------------------------
+#  Manejo de errores HTML
+# -------------------------
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse(
+        "error.html",
+        {"request": request, "status_code": exc.status_code, "detail": exc.detail},
+        status_code=exc.status_code,
+    )
